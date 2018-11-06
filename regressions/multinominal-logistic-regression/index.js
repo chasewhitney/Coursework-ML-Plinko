bdadf6e584c3843eecfdad4afaf49469237929b4
Chase Whitney <ChaseEWhitney@gmail.com>
2018-11-06 17:21:53 -0600

    added folder for multinomial logistic regression

---
+++ regressions/multinominal-logistic-regression/index.js
@@ -0,0 +1,42 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
+const plot = require('node-remote-plot');
+
+const { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
+  dataColumns: ['horsepower', 'displacement', 'weight'],
+  labelColumns: ['mpg'],
+  shuffle: true,
+  splitTest: 50,
+  converters: {
+    mpg: value => {
+      const mpg = parseFloat(value);
+      if(mpg < 15) {return [1,0,0]}
+      else if(mpg < 30) {return [0,1,0]}
+      else {return [0,0,1]}
+
+    }
+  }
+});
+
+console.log(labels);
+
+// const regression = new LogisticRegression(features, labels, {
+//   learningRate: 0.5,
+//   iterations: 100,
+//   batchSize: 50,
+//   decisionBoundary: 0.5
+// });
+//
+// regression.train();
+//
+// // regression.predict([
+// //   ['130', '307', '1.75'],
+// //   ['88', '97', '1.065']
+// // ]).print();
+//
+// console.log(regression.test(testFeatures, testLabels));
+// plot({
+//   x: regression.costHistory.reverse()
+// });
